cf973935041d3b626dc3eced33ebfee893408586
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-04-12 12:35:54 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/noteapp_internship/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.noteapp_internship;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:noteapp_internship/screens/splashpage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashPage(),
+    );
+  }
+}
