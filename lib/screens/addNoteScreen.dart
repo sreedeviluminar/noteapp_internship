58c123a8b7d3372e21c9d1efde99b2a7d1cdc738
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-04-17 14:46:54 +0530

    initial commit

--- lib/screens/addNoteScreen.dart
+++ lib/screens/addNoteScreen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp_internship/screens/home_note.dart';
 import 'package:noteapp_internship/utils/appcolors.dart';
 
 import '../utils/textConstants.dart';
 
 class AddNoteScreen extends StatefulWidget {
+  /// create a initial state or mutable state of this screen
   @override
   State<AddNoteScreen> createState() => _AddNoteScreenState();
 }
 
 class _AddNoteScreenState extends State<AddNoteScreen> {
+  ///used to fetch values from textfield
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+  TextEditingController _addItemController = TextEditingController();
+
+  ///list is to store data from add item field as list
+  List<String> checkListItems = [];
+
+  /// to fetch state of checklist item
+  List<bool> _isChecked = [];
   bool isCheckListEnabled = false;
+
+  ///to monitor the state of this form which is valid or not
+  var formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +37,7 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Form(
+            key: formkey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -30,11 +46,19 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
                   style: AppTextTheme.bodyTextStyle,
                 ),
                 TextFormField(
-                  decoration: InputDecoration(
+                  validator: (title) {
+                    if (title!.isEmpty) {
+                      return "Please Enter a Title.";
+                    } else {
+                      return null;
+                    }
+                  },
+                  controller: _titleController,
+                  decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: "Enter your Title"),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
                 Text(
@@ -42,8 +66,9 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
                   style: AppTextTheme.bodyTextStyle,
                 ),
                 TextFormField(
+                  controller: _contentController,
                   maxLines: 8,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: "Enter your Content"),
                 ),
@@ -70,12 +95,13 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
                         children: [
                           Expanded(
                             child: TextField(
-                              decoration: InputDecoration(
+                              controller: _addItemController,
+                              decoration: const InputDecoration(
                                   border: OutlineInputBorder(),
                                   hintText: "Add Item"),
                             ),
                           ),
-                          SizedBox(
+                          const SizedBox(
                             width: 20,
                           ),
                           CircleAvatar(
@@ -87,33 +113,38 @@ class _AddNoteScreenState extends State<AddNoteScreen> {
                                   color: AppColor.headTextTheme,
                                 )),
                           )
-                          ],
-                        ),
-                      ],
-                    ),
-                ],
-              ),
+                        ],
+                      ),
+                    ],
+                  ),
+              ],
             ),
           ),
         ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: FloatingActionButton.extended(
-            backgroundColor: AppColor.basicTheme,
-            onPressed: () {},
-            label: Text(
-              "Add Note",
-              style: AppTextTheme.appBarTextStyle,
-            ),
-            icon: Icon(
-              Icons.note_add_outlined,
-              size: 20,
-              color: AppColor.headTextTheme,
-            ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FloatingActionButton.extended(
+          backgroundColor: AppColor.basicTheme,
+          onPressed: () {
+            var valid = formkey.currentState!.validate();
+            if (valid == true) {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => NoteHome()));
+            }
+          },
+          label: Text(
+            "Add Note",
+            style: AppTextTheme.appBarTextStyle,
+          ),
+          icon: Icon(
+            Icons.note_add_outlined,
+            size: 20,
+            color: AppColor.headTextTheme,
           ),
         ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
     );
   }
 }
